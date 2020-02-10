757a9a431bd05eaa424df2f5a614bfa9684962ee
lizhavird <60331359+lizhavird@users.noreply.github.com>
2020-02-10 09:34:37 -0800

    Add files via upload
    week-1-assessment: attempted to install jest on my windows, but was unable to test

--- js-intro.js
+++ js-intro.js
@@ -7,55 +7,153 @@
 // --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Farhenheit.
 // Use the test variables provided below. Expected outcome: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
-
+//212 f = boiling point
+//
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
+//
+// function testTemp1() {
+//
+//
+//   if
+//     (temp1 < 212) {
+//       return `${temp1} is below boiling point`
+//   }  else if
+//     (tempt1 ===212) {
+//       return `${temp1} is at boiling point`
+//   }
+//   else if(temp1> 212) {
+//       return `${temp1} is above boiling point`}
+// }
+// function testTemp2() {
+//       if
+//         (temp2 < 212) {
+//           return `${temp2} is below boiling point`
+//       }  else if
+//         (temp2 ===212) {
+//           return `${temp2} is at boiling point`
+//       }
+//       else if(temp2>212) {
+//           return `${temp2} is above boiling point`}
+//
+// }
+//
+// function testTemp3() {
+//       if
+//         (temp3 < 212) {
+//           return `${temp3} is below boiling point`
+//       }  else if
+//         (temp3 === 212) {
+//           return `${temp3} is at boiling point`
+//       }
+//       else if(temp3 > 212) {
+//           return `${temp3} is above boiling point`}
+//
+// }
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
+//
+// console.log(testTemp1())
+// console.log(testTemp2())
+// console.log(testTemp3())
+//
 
 
 
 // --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
 // Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45]
-
-var myNumbers1 = [3, 7, 0, 6, -9]
-
-
+//
+// var myNumbers1 = [3, 7, 0, 6, -9]
+//
+//
+// const mult5 = (array) => {
+//   let newArr = []
+//   for(let i=0; i<array.length; i++){
+//     newArr.push(array[i] * 5)
+//   }
+//   return newArr
+// }
+//
+// console.log(mult5(myNumbers1))
+//
+//
+//
+//
 
 
 // --------------------3) Create a function that multiplies each number in the array by 5 using map.
 // Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
 
-var myNumbers2 = [8, -7, 0, 6, 2]
-
-
-
+// var myNumbers2 = [8, -7, 0, 6, 2].map(function(x) { return x * 5; });
+//
+// console.log(myNumbers2)
 
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
+//need help with this problem
 
-
-
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
+//
+// var spltstring = stringWithVowels1.split('')
+// var spltstring2 = stringWithVowels2.split('')
+//
+// removeVowels = (array) => {
+//
+//   let noVowels = array.filter value (!===) "a","e","i","o","u")
+//   return noVowels
+// }
+// console.log(removeVowels(stringWithVowels1))
 
 
 // --------------------5) Expand the logic from #4 to inform your user if the variable passed into the vowel removal function is not a string.
 // Copy and paste or rewrite the function. Use the test variables provided below.
-
-var notAString1 = true
-var notAString2 = 42
-
-
+//
+// var notAString1 = true
+// var notAString2 = 42
+//
+//
 
 
 // --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
 // Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
-
-var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
-
-
+//
+// var toonimals = [
+//   {name: "Itchy", animal: "mouse"},
+//   {name: "Stimpy", animal: "cat"},
+//   {name: "Daffy", animal: "duck"},
+//   {name: "Scratchy", animal: "cat"},
+//   {name: "Ren", animal: "dog"},
+//   {name: "Felix", animal: "cat"}]
+//
+//
+// var animal = toonimals.filter(value => value.animal === "cat")
+// console.log(animal)
 
 // --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
 // Expected output: "Itchy" "Daffy" "Ren"
+
+// not sure how to phrase the return value
+
+// var toonimals = [
+//   {name: "Itchy", animal: "mouse"},
+//   {name: "Stimpy", animal: "cat"},
+//   {name: "Daffy", animal: "duck"},
+//   {name: "Scratchy", animal: "cat"},
+//   {name: "Ren", animal: "dog"},
+//   {name: "Felix", animal: "cat"}]
+//
+// var { name, animal } = toonimals
+//
+// let nonCats=toonimals.filter(val=>{
+//   return val.animal !=== "cat"
+// })
+//
+// console.log(nonCats)
+//
+//
+// console.log(animal)
--- js-testing.test.js
+++ js-testing.test.js
@@ -10,21 +10,41 @@
 
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
-
-
-
-
+//was unable to run jest on my windows--none of tests have been tested 
+describe("addThemup", () => {
+  test ("takes two numbers(num1 & num2) and returns the sum of num1 & num2", () => {
+    expect(addThemUp(num1,num2)).toEqual(num1+num2)
+  })
+})
 
 // --------------------1b) See the test fail. THEN write the code to make the test pass.
 
+var num1 =5
+var num2 =6
+var addThemup= () => {
+  return [num1 + num2]
+}
 
+console.log(addThemup())
 
 
 
 // --------------------2a) Create a test for a function called triangleArea that takes the base and the height of a triange and returns the area. The area of a triange is the base times the height divided by two.
-
-
+//The area of a triange is the base times the height divided by two
+describe("triangleArea", () => {
+  test ("takes the base & height of a triangle and returns the area.", () => {
+    expect(triangleArea)).toEqual(b*h/2)
+  })
+})
 
 
 
 // --------------------2b) See the test fail. THEN write the code to make the test pass.
+
+var base=5
+var height =5
+var triangleArea= () => {
+  return base * height / 2
+}
+
+console.log(triangleArea())
